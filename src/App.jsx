e9a0e853a95aa9a777643117bd814d7feeadc203
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-03-24 18:16:24 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='bg-red-600 text-xl font-extrabold'>ba ba</p>
+      <h2>dkjfksdjfjsoidjfklsdjfkl</h2>
+    </>
+  )
+}
+
+export default App
