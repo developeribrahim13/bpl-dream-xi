505b23ef5add5dcb56c41702522a954f90544081
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-03-25 23:11:07 +0600

    finished card component design

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -1,21 +1,27 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import AvailablePlayers from './availableplayers/AvailablePlayers';
+import SelectedPlayers from './selectedplayers/SelectedPlayers';
+import { SeparatorVerticalIcon } from 'lucide-react';
+
+const Players = () => {
 
 const fetchPlayers = fetch('/public/players.json')
 .then(res => res.json())
 
-const Players = () => {
+const [available, setAvailable] = useState(true)
+
     return (
         <div className='max-w-[90%] mx-auto font-sora'>
-            <div className='flex justify-between items-center'>
+            <div className='flex justify-between items-center mb-6'>
                 <p className='text-[28px] font-bold'>Available Players</p>
                 <div className='flex'>
-                    <button className='btn bg-lime-300'>Available</button>
-                    <button className='btn'>Selected (0)</button>
+                    <button onClick={()=>setAvailable(true)} className={`btn ${available?"bg-lime-300":""}`}>Available</button>
+                    <button onClick={()=>setAvailable(false)} className={`btn ${!available?"bg-lime-300":""}`}>Selected (0)</button>
                 </div>
             </div>
             <Suspense fallback={<p>Players Loading.....</p>}>
-            <AvailablePlayers fetchPlayers={fetchPlayers}/>
+            {available?<AvailablePlayers fetchPlayers={fetchPlayers}/>:
+            <SelectedPlayers></SelectedPlayers>}
             </Suspense>
         </div>
     );
--- src/components/players/availableplayers/card/Card.jsx
+++ src/components/players/availableplayers/card/Card.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const Card = ({Players}) => {
+const Card = ({ Players }) => {
     return (
-        <div>
+        <div className='grid grid-cols-3 grid-rows-1 gap-4 '>
             {Players.map(player => {
-                console.log(player)
-            }
+            return (
+                <div key={player.id} className="card bg-base-100 w-96 shadow-cyan-200 shadow-xs">
+                    <figure className='w-full h-[250px]'>
+                        <img
+                            className='w-full h-full object-cover'
+                            src={player.player_img}
+                            alt={player.player_name}/>
+                    </figure>
+                    <div className='p-2 space-y-1'>
+                        <h1 className='font-bold text-xl'>{player.player_name}</h1>
+                            <div className='flex gap-2 items-center'>
+                                <img src={player.country_flag_img} width={20}></img>
+                                <p className='font-medium'>{player.country_name}</p>
+                            </div>
+                            <div className='flex justify-between'>
+                            <p className='text-green-600 font-semibold'>{player.player_role}</p>
+                            <p>Rating: {player.rating}</p>
+                            </div>
+                            <div className='flex justify-between'>
+                            <p>{player.bat_style}</p>
+                            <p>{player.bowl_style}</p>
+                            </div>
+                            <div className='flex justify-between items-center'>
+                            <p className='font-medium text-red-900'> Price: ${player.price}</p>
+                            <button className='btn'>Choose Player</button>
+                            </div>
+                    </div>
+                </div>
+              )}
             )}
         </div>
     );
